8786071563f4b137627eb27515072a57dd4f05ea
TrevorSundberg <Trevor.Sundberg@gmail.com>
2024-12-04 22:19:03 -0800

    More comprehensive setup script

--- models.sh
+++
@@ -1,2 +0,0 @@
-pip install -U "huggingface_hub[cli]"
-huggingface-cli download sentence-transformers/all-MiniLM-L6-v2 --local-dir ./models/sentence-transformers_all-MiniLM-L6-v2 --local-dir-use-symlinks False
