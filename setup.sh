a25db7bc3983f789272bf28c4cbe2ba841f00954
TrevorSundberg <Trevor.Sundberg@gmail.com>
2024-12-09 09:34:07 -0800

    Fix curl command with redirects on huggingface

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 npm i
 pip install -U "huggingface_hub[cli]"
 huggingface-cli download sentence-transformers/all-MiniLM-L6-v2 --local-dir ./models/sentence-transformers_all-MiniLM-L6-v2 --local-dir-use-symlinks False
-curl https://huggingface.co/NousResearch/Nous-Hermes-2-Mistral-7B-DPO-GGUF/resolve/main/Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf --output "./models/Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf"
+curl -L https://huggingface.co/NousResearch/Nous-Hermes-2-Mistral-7B-DPO-GGUF/resolve/main/Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf --output "./models/Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf"
 ./docker.sh
 
